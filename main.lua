3c3845de4fa800b0d545b1c09ad68b38b7e4a54a
vunje <gustavofraguas@gmail.com>
2024-09-22 22:45:17 -0300

    tentando colocar um log

--- main.lua
+++ main.lua
@@ -1,24 +1,42 @@
 -- Sti= require("plugins.Simple-Tiled-Implementation.sti")
 
-local Gamera = require("plugins.gamera.gamera")
-local camera = Gamera.new(0,0,2000,2000)
+local Cupid = require("plugins.cupid-git.cupid")
 
+local Gamera = require("plugins.gamera.gamera")
+local camera = Gamera.new(0,0,800,600) 
 -- Camera = require("plugins.lovelibs.CameraMgr.lib.CameraMgr")
 -- CM = require("plugins.lovelibs.CameraMgr.lib.CameraMgr").newManager()
 -- CM=Camera.newManager()
 -- BK=Camera.newManager()
 
+local Baton = require("plugins.baton.baton")
+local input = Baton.new {
+    controls = {
+        left = {'key:left', 'key:a', 'axis:leftx-', 'button:dpleft'},
+        right = {'key:right', 'key:d', 'axis:leftx+', 'button:dpright'},
+        up = {'key:up', 'key:w', 'axis:lefty-', 'button:dpup'},
+        down = {'key:down', 'key:s', 'axis:lefty+', 'button:dpdown'},
+        action = {'key:x', 'button:a'},
+    },
+    pairs = {
+        move = {'left', 'right', 'up', 'down'}
+    },
+    joystick = love.joystick.getJoysticks()[1],
+}
+
+
 local editgrid = require("plugins.Editgrid.editgrid")
+
 local visuals = {
     -- size = 100,
-    -- subdivisions = 5,
+    subdivisions = 3,
     -- color = {128, 140, 250},
-    -- drawScale = false,
-    -- xColor = {255, 255, 0},
-    -- yColor = {0, 255, 255},
-    -- fadeFactor = 0.3,
+    drawScale = true,
+    xColor = {255, 255, 0},
+    yColor = {0, 255, 255},
+    fadeFactor = 0.3,
     -- textFadeFactor = 0.5,
-    -- hideOrigin = true,
+    hideOrigin = false,
     interval = 50
 }
 
@@ -33,7 +51,6 @@ local visuals = {
 --     sh = love.graphics.getHeight() - 10
 -- }
 
-
 function love.load()
 
     -- fs=love.window.setFullscreen(true,"desktop")
@@ -42,9 +59,14 @@ function love.load()
     size = (width<height) and width or height
 
     bkg=love.graphics.newImage("assets/img/bk_red01.jpg")
-    image=love.graphics.newImage("assets/img/img_rosarioSaoMiguel.png")
+    image=love.graphics.newImage("assets/img/img_rosarioSaoMiguelHigh.png")
+    sombra=love.graphics.newImage("assets/img/img_rosarioSaoMiguelSombra.png");
+    crosshair=love.graphics.newImage("assets/img/img_crosshair2.png")
+
     quad = love.graphics.newQuad(0, 0, size, size, size, size)
+    quad2 = love.graphics.newQuad(-10,-10, size, size, size, size)
     quad1 = love.graphics.newQuad(0, 0, width, height, width, height)
+    q_crosshair = love.graphics.newQuad(0, 0, 15, 15, 15, 15)
 
     -- CM.setCoords(size/2, size/2)
     -- BK.setOffset(20)
@@ -60,39 +82,23 @@ function love.load()
 end
 
 function love.update(dt)
-    -- CM.update(dt)
-    -- BK.update(dt)
+    input:update()
+    -- camera:setAngle(r)
     s=s+0.001
     r=r+0.01
     l=l+0.01
 end
 
 function love.draw()
-    -- CM.setScale(s)
-    -- CM.setRotation(r)
-    
-    -- BK.setRotation(r)
-    -- CM.setLerp(l)
-    -- CM.setLerp(l)
-
-    -- BK.attach()
-    --     love.graphics.draw(bkg, quad1, 0, 0)
-    -- BK.detach()
-
-    -- CM.attach()
-        -- map:draw()
-    camera:setAngle(r)
 
     camera:draw(function(l,t,w,h)
+        -- map:draw()
         love.graphics.draw(bkg, quad1, 0, 0)
+        love.graphics.draw(sombra, quad2, (width/2-size/2), (height/2-size/2))
         love.graphics.draw(image, quad, (width/2-size/2), (height/2-size/2))
-        -- love.graphics.draw(image, quad, 0, 0)
+        love.graphics.draw(crosshair, q_crosshair, 0, 0)
     end)
-    -- CM.detach()
-    -- CM.debug()
-    
-    -- editgrid.draw(CM, visuals)
-    -- editgrid.draw(camera, visuals)
+    editgrid.draw(camera, visuals)
 end
 
 function love.keyreleased(key)
